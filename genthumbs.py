fe829ce69292bed984d76fd8358d09455bd0017f
freddie <freddie@35f662cf-58f2-0310-8820-d172d141b225>
2005-08-22 07:23:14 +0000

    Bump copyright date here too.

--- genthumbs.py
+++ genthumbs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2004, Freddie
+# Copyright (c) 2004-2005, Freddie
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
