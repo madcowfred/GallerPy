920897b1205db9f0329fcf382c724a0a1b838cd5
Jan Cerny <cernyhonza@gmail.com>
2010-08-29 00:29:10 -0700

    Fixes incorrect indentation.

--- gallerpy.py
+++ gallerpy.py
@@ -166,12 +166,12 @@ def generate_thumbnails(Conf, root, files, sizes=1):
 			
 			# If it's not a truecolor image, make it one
 			if Conf['thumb_jpeg'] and img.mode != 'RGB':
-                try:
-				    img = img.convert('RGB')
-                except Exception, msg:
-                    warning = "Warning: failed to convert '%s' - %s" % (image_name, msg)
-                    warnings.append(warning)
-                    continue
+				try:
+					img = img.convert('RGB')
+				except Exception, msg:
+					warning = "Warning: failed to convert '%s' - %s" % (image_name, msg)
+					warnings.append(warning)
+					continue
 			
 			# Thumbnail it
 			try:
